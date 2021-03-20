5e13d13e75a3111cabca79f817a74302d68c998d
salvinanto7 <salvinanto7@gmail.com>
2021-03-20 10:33:50 +0530

    added ckt dig , bloick dig and primary code for the project

---
+++ speed_display.ino
@@ -0,0 +1,110 @@
+#include <TinyGPS++.h>
+#include<SoftwareSerial.h>
+#include<Wire.h>
+#include<Adafruit_SH1106.h>
+
+#define OLED_ADDRESS 0x3C
+#define OLED_RESET -1
+#define vibrator_sensor 11
+
+void display_speed();
+String lat_val , long_val, hour_val , mins_val, sec_val;
+Adafruit_SH1106 display(OLED_RESET);
+TinyGPSPlus gps;
+SoftwareSerial gpssoft(2,3);  //rx=2 ,tx=3
+SoftwareSerial gsm900(14,15);  //rx=14  tx=15
+
+void setup(){
+  Serial.begin(9600);
+  gpssoft.begin(9600);
+  gsm900.begin(9600);
+  pinMode(vibrator_sensor,INPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  display.begin(OLED_ADDRESS);      // removed a line SH_1106_SWITCHCAPVACC, from begin  : uswed to gnerae 3.3v INTERNALLY. connect the vcc of ole to 3.3v terminal of 3.3v of nano
+  display.clearDisplay();
+  display.display();
+}
+
+void loop(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(27,2);
+  display.print("SPEED (KMS)");
+  display.display();
+  while(gpssoft.available()>0){
+    if (gps.encode(gpssoft.read()))
+      display_speed();
+    else if(millis() >5000 && gps.charsProcessed()<10){   //checks whether the gps is runnig for 5 seconds and recieved data less than 10 characters
+      display.setTextSize(1);
+      display.setCursor(35,40);
+      display.print("ERROR IN COLLECTING DATA!!");
+      display.display();
+    }
+  }
+  if(vibrator_sensor ==1){
+    getlocation();
+    gsm900.println("AT+CMGF=1");
+    gsm900.println("AT+CMGS=\"+919875641230\"");  //receipent number 1 
+    gsm900.print("PLEASE HELP ,  I MET WITH AN ACCIDENT!");
+    gsm900.print("my location:");
+    gsm900.print("https://www.google.com/maps/@");
+    gsm900.println(lat_val);
+    gsm900.println(",");
+    gsm900.println(long_val);
+    gsm900.println(",14z");
+    gsm900.print("time:");
+    gsm900.println(hour_val);
+    gsm900.println(":");
+    gsm900.println(mins_val);
+    gsm900.println(":");
+    gsm900.println(sec_val);
+    gsm900.write(26);
+    digitalWrite(12,HIGH);   //denotes the end of transimission of ms g to first no.
+
+    gsm900.println("AT+CMGF=1");
+    gsm900.println("AT+CMGS=\"+919876543210\"");  //receipent number 2 
+    gsm900.print("PLEASE HELP ,  I MET WITH AN ACCIDENT!");
+    gsm900.print("my location:");
+    gsm900.print("https://www.google.com/maps/@");
+    gsm900.println(lat_val);
+    gsm900.println(",");
+    gsm900.println(long_val);
+    gsm900.println(",14z");
+    gsm900.print("time:");
+    gsm900.println(hour_val);
+    gsm900.println(":");
+    gsm900.println(mins_val);
+    gsm900.println(":");
+    gsm900.println(sec_val);
+    gsm900.write(26);
+    digitalWrite(13,HIGH);    //denotes the end of transmission of msg to 2nd no.
+  }
+}
+
+void display_speed(){
+  if(gps.speed.isValid()){
+    display.setTextSize(1);
+    display.setCursor(35,40);
+    display.print(gps.speed.kmph());
+    display.display();
+  }
+  else{
+    display.setTextSize(1);
+    display.setCursor(35,40);
+    display.print("NO DATA!!");
+    display.display();
+  }
+}
+
+void getlocation(){
+  lat_val = gps.location.lat(); 
+  long_val= gps.location.lng();
+  if (gps.time.isValid()) {
+        hour_val = gps.time.hour();
+        mins_val = gps.time.minute();
+        sec_val  = gps.time.second();
+      }
+  return lat_val , long_val ,hour_val , mins_val, sec_val;
+}
